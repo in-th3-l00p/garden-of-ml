48c10789c1286f530e9e91c53d27c2cd8eacee16
Tisca Catalin <admin@tiscacatalin.com>
2025-11-29 14:00:43 +0200

    initialized tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
 	// Enable React to support React JSX components.
-	integrations: [react()],
+	integrations: [react(), tailwindcss()],
 });
